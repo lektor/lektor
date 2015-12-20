ff4ac5db3d76bc381047cfce1d2187d6086ce8aa
Armin Ronacher <armin.ronacher@active-4.com>
2015-12-20 13:41:12 +0100

    Added search

--- docs/webpack/js/app.js
+++ docs/webpack/js/app.js
@@ -1,4 +1,5 @@
 require('bootstrap');
+var qs = require('query-string');
 
 function selectText(text) {
   if (document.body.createTextRange) {
@@ -88,7 +89,55 @@ function initInstallRow() {
   }
 }
 
+function initGoogleSearch() {
+  var container = $('.google-custom-search');
+  if (container.length == 0) {
+    return;
+  }
+  var cx = '012722186170730423054:utwznhnrrmi';
+  var gcse = document.createElement('script');
+  gcse.type = 'text/javascript';
+  gcse.async = true;
+  gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+      '//cse.google.com/cse.js?cx=' + cx;
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(gcse, s);
+
+  $(`
+    <gcse:searchresults-only linktarget="_parent"></gcse:searchresults-only>
+  `).appendTo(container);
+  $(`
+  <div style="display: none">
+    <div id="base_webResult">
+      <div class="gs-webResult gs-result"
+        data-vars="{
+          longUrl: function() {
+            var i = unescapedUrl.indexOf(visibleUrl);
+            return i < 1 ? visibleUrl : unescapedUrl.substring(i);
+          },
+          processSearchTitle: function(title) {
+            return title.split(' | ').slice(0, -2).join(' | ') || 'Documentation';
+          }
+        }">
+        <div class="gs-title">
+          <a class="gs-title" data-attr="{href:unescapedUrl, target:target}"
+            data-body="html(processSearchTitle(title))"></a>
+        </div>
+        <div class="gs-visibleUrl gs-visibleUrl-long" data-body="longUrl()"></div>
+        <div class="gs-snippet" data-body="html(content)"></div>
+      </div>
+    </div>
+  </div>
+  `).appendTo(container);
+
+  var params = qs.parse(location.search);
+  if (params.q) {
+    $('input[name="q"]', container).val(params.q);
+  }
+}
+
 $(function() {
   initDownloadButton();
   initInstallRow();
+  initGoogleSearch();
 });
