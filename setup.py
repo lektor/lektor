adbd7b3c0e3ef384e5a1304719c1172968065e9c
Armin Ronacher <armin.ronacher@active-4.com>
2015-12-21 20:45:40 +0100

    This is 1.1-dev

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Lektor',
-    version='1.0',
+    version='1.1-dev',
     url='http://github.com/lektor/lektor/',
     description='A static content management system.',
     license='BSD',
