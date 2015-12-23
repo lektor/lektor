9a8d9c51c401a3c3da3cab418c3d41588026a577
Armin Ronacher <armin.ronacher@active-4.com>
2015-12-23 20:32:32 +0100

    Added tests for pagination.items

--- tests/test_db.py
+++ tests/test_db.py
@@ -10,91 +10,6 @@ def test_root(pad):
     assert record['_path'] == '/'
 
 
-def test_paginated_children(pad):
-    page1 = pad.get('/projects', page_num=1)
-
-    assert page1 is not None
-    assert page1['_model'] == 'projects'
-    assert page1['_template'] == 'projects.html'
-
-    assert page1.datamodel.pagination_config.per_page == 4
-
-    assert page1.children.count() == 7
-    assert page1.page_num == 1
-    assert page1.pagination.items.count() == 4
-
-    children = page1.pagination.items.all()
-    assert len(children) == 4
-    assert [x['name'] for x in children] == [
-        u'Bagpipe',
-        u'Coffee',
-        u'Master',
-        u'Oven',
-    ]
-
-    assert ('projects', '_primary', 1) in pad.cache.persistent
-    assert ('projects', '_primary', 2) not in pad.cache.persistent
-
-    page2 = pad.get('/projects', page_num=2)
-
-    assert page2.children.count() == 7
-    assert page2.page_num == 2
-    assert page2.pagination.items.count() == 3
-
-    children = page2.pagination.items.all()
-    assert len(children) == 3
-    assert [x['name'] for x in children] == [
-        u'Postage',
-        u'Slave',
-        u'Wolf',
-    ]
-
-    assert ('projects', '_primary', 2) in pad.cache.persistent
-
-
-def test_unpaginated_children(pad):
-    page_all = pad.get('/projects')
-
-    assert page_all
-
-    assert page_all.pagination.items.count() == 7
-    assert page_all.page_num is None
-
-    children = page_all.pagination.items.all()
-    assert len(children) == 7
-    assert [x['name'] for x in children] == [
-        u'Bagpipe',
-        u'Coffee',
-        u'Master',
-        u'Oven',
-        u'Postage',
-        u'Slave',
-        u'Wolf',
-    ]
-
-
-def test_pagination_access(pad):
-    page = pad.get('/projects', page_num=1)
-
-    assert page.pagination.page == 1
-    assert page.pagination.next.pagination.page == 2
-    assert page.pagination.for_page(2).pagination.page == 2
-
-    assert page.pagination.for_page(0) is None
-    assert page.pagination.for_page(3) is None
-
-
-def test_url_matching_for_pagination(pad):
-    page1 = pad.resolve_url_path('/projects/')
-    assert page1.page_num == 1
-
-    page2 = pad.resolve_url_path('/projects/page/2/')
-    assert page2.page_num == 2
-
-    page1_explicit = pad.resolve_url_path('/projects/page/1/')
-    assert page1_explicit is None
-
-
 def test_project_implied_model(pad):
     project = pad.query('/projects').first()
     assert project is not None
@@ -110,42 +25,6 @@ def test_child_query_visibility_setting(pad):
     assert project_query._include_undiscoverable
 
 
-def test_pagination_url_paths(pad):
-    # Even though this is paginated, getting to the non paginated version
-    # just looks like going to the first page.  We do this because it
-    # is more convenient for usage.
-    project = pad.get('/projects')
-    assert project.url_path == '/projects/'
-
-    # However first page looks the same
-    project = pad.get('/projects', page_num=1)
-    assert project.url_path == '/projects/'
-
-    # second page is different
-    project = pad.get('/projects', page_num=2)
-    assert project.url_path == '/projects/page/2/'
-
-
-def test_unpaginated_children_other_alt(pad):
-    page_all = pad.get('/projects', alt='de')
-
-    assert page_all.pagination.items.count() == 8
-    assert page_all.page_num is None
-
-    children = page_all.pagination.items.all()
-    assert len(children) == 8
-    assert [x['name'] for x in children] == [
-        u'Dudelsack',
-        u'Kaffee',
-        u'Meister',
-        u'Ofen',
-        u'Porto',
-        u'Sklave',
-        u'Wolf',
-        u'Zaun'
-    ]
-
-
 def test_alt_fallback(pad):
     # page that is missing a german tranlation
     wolf_page = pad.get('/projects/wolf', alt='de')
@@ -154,19 +33,6 @@ def test_alt_fallback(pad):
     assert wolf_page.alt == 'de'
     assert wolf_page['_source_alt'] == '_primary'
     assert wolf_page['name'] == 'Wolf'
-
-
-def test_url_matching_for_alt_pagination(pad):
-    page1 = pad.resolve_url_path('/de/projects/')
-    assert page1.alt == 'de'
-    assert page1.page_num == 1
-
-    page2 = pad.resolve_url_path('/de//projects/page/2/')
-    assert page2.alt == 'de'
-    assert page2.page_num == 2
-
-    page1_explicit = pad.resolve_url_path('/de/projects/page/1/')
-    assert page1_explicit is None
 
 
 def test_url_matching_with_customized_slug_in_alt(pad):
---
+++ tests/test_pagination.py
@@ -0,0 +1,146 @@
+def test_paginated_children(pad):
+    page1 = pad.get('/projects', page_num=1)
+
+    assert page1 is not None
+    assert page1['_model'] == 'projects'
+    assert page1['_template'] == 'projects.html'
+
+    assert page1.datamodel.pagination_config.per_page == 4
+
+    assert page1.children.count() == 7
+    assert page1.page_num == 1
+    assert page1.pagination.items.count() == 4
+
+    children = page1.pagination.items.all()
+    assert len(children) == 4
+    assert [x['name'] for x in children] == [
+        u'Bagpipe',
+        u'Coffee',
+        u'Master',
+        u'Oven',
+    ]
+
+    assert ('projects', '_primary', 1) in pad.cache.persistent
+    assert ('projects', '_primary', 2) not in pad.cache.persistent
+
+    page2 = pad.get('/projects', page_num=2)
+
+    assert page2.children.count() == 7
+    assert page2.page_num == 2
+    assert page2.pagination.items.count() == 3
+
+    children = page2.pagination.items.all()
+    assert len(children) == 3
+    assert [x['name'] for x in children] == [
+        u'Postage',
+        u'Slave',
+        u'Wolf',
+    ]
+
+    assert ('projects', '_primary', 2) in pad.cache.persistent
+
+
+def test_unpaginated_children(pad):
+    page_all = pad.get('/projects')
+
+    assert page_all
+
+    assert page_all.pagination.items.count() == 7
+    assert page_all.page_num is None
+
+    children = page_all.pagination.items.all()
+    assert len(children) == 7
+    assert [x['name'] for x in children] == [
+        u'Bagpipe',
+        u'Coffee',
+        u'Master',
+        u'Oven',
+        u'Postage',
+        u'Slave',
+        u'Wolf',
+    ]
+
+
+def test_pagination_access(pad):
+    page = pad.get('/projects', page_num=1)
+
+    assert page.pagination.page == 1
+    assert page.pagination.next.pagination.page == 2
+    assert page.pagination.for_page(2).pagination.page == 2
+
+    assert page.pagination.for_page(0) is None
+    assert page.pagination.for_page(3) is None
+
+
+def test_url_matching_for_pagination(pad):
+    page1 = pad.resolve_url_path('/projects/')
+    assert page1.page_num == 1
+
+    page2 = pad.resolve_url_path('/projects/page/2/')
+    assert page2.page_num == 2
+
+    page1_explicit = pad.resolve_url_path('/projects/page/1/')
+    assert page1_explicit is None
+
+
+def test_pagination_url_paths(pad):
+    # Even though this is paginated, getting to the non paginated version
+    # just looks like going to the first page.  We do this because it
+    # is more convenient for usage.
+    project = pad.get('/projects')
+    assert project.url_path == '/projects/'
+
+    # However first page looks the same
+    project = pad.get('/projects', page_num=1)
+    assert project.url_path == '/projects/'
+
+    # second page is different
+    project = pad.get('/projects', page_num=2)
+    assert project.url_path == '/projects/page/2/'
+
+
+def test_unpaginated_children_other_alt(pad):
+    page_all = pad.get('/projects', alt='de')
+
+    assert page_all.pagination.items.count() == 8
+    assert page_all.page_num is None
+
+    children = page_all.pagination.items.all()
+    assert len(children) == 8
+    assert [x['name'] for x in children] == [
+        u'Dudelsack',
+        u'Kaffee',
+        u'Meister',
+        u'Ofen',
+        u'Porto',
+        u'Sklave',
+        u'Wolf',
+        u'Zaun'
+    ]
+
+
+def test_url_matching_for_alt_pagination(pad):
+    page1 = pad.resolve_url_path('/de/projects/')
+    assert page1.alt == 'de'
+    assert page1.page_num == 1
+
+    page2 = pad.resolve_url_path('/de//projects/page/2/')
+    assert page2.alt == 'de'
+    assert page2.page_num == 2
+
+    page1_explicit = pad.resolve_url_path('/de/projects/page/1/')
+    assert page1_explicit is None
+
+
+def test_pagination_items_filter(pad):
+    blog = pad.get('/blog', page_num=1)
+    assert blog.children.count() == 2
+    assert sorted(x['_id'] for x in blog.children) == [
+        'dummy.xml', 'post1']
+
+    assert blog.pagination.items.count() == 1
+    assert blog.pagination.items.first()['_id'] == 'post1'
+
+    dummy = blog.children.get('dummy.xml')
+    assert dummy is not None
+    assert dummy['_model'] == 'none'
