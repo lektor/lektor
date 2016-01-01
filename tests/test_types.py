eabeb0a0a823c00b1073efbd4114858b1c60e6d7
Armin Ronacher <armin.ronacher@active-4.com>
2016-01-01 21:46:06 +0100

    Added more tests

--- tests/test_types.py
+++ tests/test_types.py
@@ -1,6 +1,7 @@
 from lektor.datamodel import Field
 from lektor.markdown import Markdown
 from lektor.context import Context
+from lektor.types import Undefined
 
 from markupsafe import escape, Markup
 
@@ -21,9 +22,57 @@ def test_markdown(env, pad):
         assert escape(rv) == Markup('<p>Hello <strong>World</strong>!</p>\n')
         assert rv.meta == {}
 
+        for val in '', None:
+            rv = field.deserialize_value(val, pad=pad)
+            assert isinstance(rv, Markdown)
+            assert not rv
+            assert rv.source == ''
+            assert escape(rv) == Markup('')
+            assert rv.meta == {}
+
+
+def test_string(env, pad):
+    field = make_field(env, 'string')
+
+    with Context(pad=pad):
         rv = field.deserialize_value('', pad=pad)
-        assert isinstance(rv, Markdown)
-        assert not rv
-        assert rv.source == ''
-        assert escape(rv) == Markup('')
-        assert rv.meta == {}
+        assert rv == ''
+
+        rv = field.deserialize_value(None, pad=pad)
+        assert isinstance(rv, Undefined)
+
+        rv = field.deserialize_value('foo\nbar', pad=pad)
+        assert rv == 'foo'
+
+        rv = field.deserialize_value(' 123 ', pad=pad)
+        assert rv == '123'
+
+
+def test_text(env, pad):
+    field = make_field(env, 'text')
+
+    with Context(pad=pad):
+        rv = field.deserialize_value('', pad=pad)
+        assert rv == ''
+
+        rv = field.deserialize_value(None, pad=pad)
+        assert isinstance(rv, Undefined)
+
+        rv = field.deserialize_value('foo\nbar', pad=pad)
+        assert rv == 'foo\nbar'
+
+        rv = field.deserialize_value(' 123 ', pad=pad)
+        assert rv == ' 123 '
+
+
+def test_html(env, pad):
+    field = make_field(env, 'html')
+
+    with Context(pad=pad):
+        rv = field.deserialize_value('', pad=pad)
+        assert isinstance(rv, Markup)
+        assert rv == Markup('')
+
+        rv = field.deserialize_value('<em>Foo</em>', pad=pad)
+        assert isinstance(rv, Markup)
+        assert rv == Markup('<em>Foo</em>')
