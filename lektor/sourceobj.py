9a29b94850b71b370239c3651ae22542ba214f52
Armin Ronacher <armin.ronacher@active-4.com>
2016-01-03 20:20:18 +0100

    Backed out shortcut for url to same path as it broke multi-lang pages.

--- lektor/sourceobj.py
+++ lektor/sourceobj.py
@@ -102,22 +102,16 @@ class SourceObject(object):
             path = path[1:]
 
         if resolve:
-            # If we resolve directly to the current path we can make a
-            # shortcut that always works and use the current page's url
-            # path.
-            if path in ('.', ''):
-                path = self.url_path
-            else:
-                if not path.startswith('/'):
-                    if self.path is None:
-                        raise RuntimeError('Cannot use relative URL generation '
-                                           'from sources that do not have a '
-                                           'path.  The source object without '
-                                           'a path is %r' % self)
-                    path = posixpath.join(self.path, path)
-                source = self.pad.get(path, alt=alt)
-                if source is not None:
-                    path = source.url_path
+            if not path.startswith('/'):
+                if self.path is None:
+                    raise RuntimeError('Cannot use relative URL generation '
+                                       'from sources that do not have a '
+                                       'path.  The source object without '
+                                       'a path is %r' % self)
+                path = posixpath.join(self.path, path)
+            source = self.pad.get(path, alt=alt)
+            if source is not None:
+                path = source.url_path
 
         if absolute:
             return path
