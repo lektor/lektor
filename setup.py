9dfa6cc13c54d54f822ebf36946f03be57625eb5
Armin Ronacher <armin.ronacher@active-4.com>
2016-04-12 16:45:33 +0200

    Bugfix build for bad translations. This fixes #196

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Lektor',
-    version='2.1',
+    version='2.2',
     url='http://github.com/lektor/lektor/',
     description='A static content management system.',
     license='BSD',
