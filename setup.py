0818d31d19de82559ea4c5b1d3daa6c8fcf30079
David Baumgold <david@davidbaumgold.com>
2016-05-06 00:14:28 -0400

    Indicate Python 3 compatibility

--- setup.py
+++ setup.py
@@ -32,8 +32,10 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'Topic :: Software Development :: Libraries :: Python Modules'
+        'Topic :: Software Development :: Libraries :: Python Modules',
     ],
     entry_points='''
         [console_scripts]
