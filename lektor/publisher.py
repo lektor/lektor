fb36347d74b54899b1ef697900270da9cf9ba283
David Baumgold <david@davidbaumgold.com>
2016-05-23 17:38:38 -0700

    str vs bytes

--- lektor/publisher.py
+++ lektor/publisher.py
@@ -12,7 +12,7 @@ from contextlib import contextmanager
 from werkzeug import urls
 
 from lektor._compat import (iteritems, iterkeys, range_type, string_types,
-    text_type, queue, BytesIO)
+    text_type, queue, StringIO)
 from lektor.exception import LektorException
 from lektor.utils import locate_executable, portable_popen
 
@@ -254,7 +254,7 @@ class FtpConnection(object):
         del self.log_buffer[:]
         for chunk in log:
             for line in chunk.splitlines():
-                if isinstance(line, str):
+                if not isinstance(line, text_type):
                     line = line.decode('utf-8', 'replace')
                 yield line.rstrip()
 
@@ -289,7 +289,7 @@ class FtpConnection(object):
         self.con.set_pasv(passive)
 
         try:
-            log.append(self.con.cwd(self.url.path.encode('utf-8')))
+            log.append(self.con.cwd(self.url.path))
         except Exception as e:
             log.append(str(e))
             return False
@@ -298,8 +298,8 @@ class FtpConnection(object):
         return True
 
     def mkdir(self, path, recursive=True):
-        if isinstance(path, text_type):
-            path = path.encode('utf-8')
+        if not isinstance(path, text_type):
+            path = path.decode('utf-8')
         if path in self._known_folders:
             return
         dirname, basename = posixpath.split(path)
@@ -315,9 +315,9 @@ class FtpConnection(object):
         self._known_folders.add(path)
 
     def append(self, filename, data):
-        if isinstance(filename, text_type):
-            filename = filename.encode('utf-8')
-        input = BytesIO(data)
+        if not isinstance(filename, text_type):
+            filename = filename.decode('utf-8')
+        input = StringIO(data)
         try:
             self.con.storbinary('APPE ' + filename, input)
         except Exception as e:
@@ -326,11 +326,11 @@ class FtpConnection(object):
         return True
 
     def get_file(self, filename, out=None):
-        if isinstance(filename, text_type):
-            filename = filename.encode('utf-8')
+        if not isinstance(filename, text_type):
+            filename = filename.decode('utf-8')
         getvalue = False
         if out is None:
-            out = BytesIO()
+            out = StringIO()
             getvalue = True
         try:
             self.con.retrbinary('RETR ' + filename, out.write)
@@ -345,13 +345,13 @@ class FtpConnection(object):
 
     def upload_file(self, filename, src, mkdir=False):
         if isinstance(src, string_types):
-            src = BytesIO(src)
+            src = StringIO(src)
         if mkdir:
             directory = posixpath.dirname(filename)
             if directory:
                 self.mkdir(directory, recursive=True)
-        if isinstance(filename, text_type):
-            filename = filename.encode('utf-8')
+        if not isinstance(filename, text_type):
+            filename = filename.decode('utf-8')
         try:
             self.con.storbinary('STOR ' + filename, src,
                                 blocksize=32768)
@@ -469,7 +469,7 @@ class FtpPublisher(Publisher):
             con.upload_file(tmp_dst, source, mkdir=True)
             con.rename_file(tmp_dst, artifact_name)
             con.append('.lektor/listing', '%s|%s\n' % (
-                artifact_name.encode('utf-8'), checksum
+                artifact_name, checksum
             ))
 
     def consolidate_listing(self, con, current_artifacts):
