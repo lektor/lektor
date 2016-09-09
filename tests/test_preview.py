b4f55e07947cef3ae4b3bfc56ff90f0b6934bc2b
Melissa Fabros and Patricia Arbona <pair+melissa.fabros+arbonap@gmail.com>
2016-09-08 23:19:56 -0700

    replace env argument with sratch_env, increase wait time

--- tests/test_preview.py
+++ tests/test_preview.py
@@ -1,15 +1,17 @@
 import pytest
+import mock
+
 from lektor.admin.webui import WebUI
 
 @pytest.yield_fixture(scope='function')
-def app(request, env, pad, simple_http_server):
-    yield WebUI(env, output_path=simple_http_server.document_root)
+def app(scratch_env, simple_http_server):
+    yield WebUI(scratch_env, output_path=simple_http_server.document_root)
 
 def test_preview(live_server, browser):
     test_server = live_server.url()
     
     browser.visit('{url}/dir_with_index_html'.format(url=test_server))
-    assert browser.url == '{url}/dir_with_index_html/'.format(url=test_server)
+    assert browser.url == '{url}/dir_with_index_html'.format(url=test_server)
 
     browser.visit('{url}/dir_with_index_htm/'.format(url=test_server))
     assert browser.url == '{url}/dir_with_index_htm/'.format(url=test_server)
@@ -18,7 +20,7 @@ def test_preview(live_server, browser):
     assert browser.url == '{url}/dir_with_index_htm/index.htm'.format(url=test_server)
 
     browser.visit('{url}/empty'.format(url=test_server))
-    assert browser.is_text_present('Not Found', wait_time=1)
+    assert browser.is_text_present('Not Found', wait_time=2)
 
     browser.visit('{url}/doesnt_exist'.format(url=test_server))
-    assert browser.is_text_present('Not Found', wait_time=1)
+    assert browser.is_text_present('Not Found', wait_time=2)
