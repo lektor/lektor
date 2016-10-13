9b9ab21291086989690ea4a1f5bc1ab204539015
Dr. Tarique Sani <tarique@sanisoft.com>
2016-10-13 15:30:36 +0530

    Added test for image quality

--- tests/test_images.py
+++ tests/test_images.py
@@ -62,3 +62,17 @@ def test_thumbnail_height(builder):
 
     # Thumbnail is half the original width, so its computed height is half.
     assert '<img src="./test@192.jpg" width="192" height="256">' in html
+
+def test_thumbnail_quality(builder):
+    builder.build_all()
+    image_file = os.path.join(builder.destination_path, 'test@192x256_q20.jpg')
+    image_size = os.path.getsize(image_file)
+
+    # See if the image file with said quality suffix exists 
+    assert os.path.isfile(image_file)
+    # And the filesize is less than 9000 bytes
+    assert image_size < 9000
+
+
+
+
