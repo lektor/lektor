8f9c1ec747dd19835956ec66ebb7350115edc948
Rafael Laverde <leafar91@gmail.com>
2017-10-19 19:23:50 -0500

    Add tests for loading theme variable, and theme path.

--- tests/test_themes.py
+++ tests/test_themes.py
@@ -27,6 +27,7 @@ def theme_project(theme_project_tmpdir):
     lektorfile_text = textwrap.dedent(u"""
         [project]
         name = Themes Project
+        theme = blog_theme
     """)
     theme_project_tmpdir.join("themes.lektorproject").write_text(lektorfile_text,
                                                                  "utf8",
@@ -53,5 +54,31 @@ def theme_builder(theme_pad, tmpdir):
     return Builder(theme_pad, str(tmpdir.mkdir("output")))
 
 
-def test_themes(theme_builder):
-    assert theme_builder
+def test_loading_theme_variable(theme_project):
+    assert theme_project.theme == 'blog_theme'
+
+
+def test_loading_theme_path(theme_env):
+    assert os.path.basename(theme_env.theme_path) == 'blog_theme'
+
+
+def test_loading_theme_path_if_not_setted(theme_project_tmpdir):
+    """When project doesn't have theme,
+    the first theme found in the themes folder will be loaded.
+
+    So removing blog_theme will cause project_theme to be loaded.
+    """
+    from lektor.project import Project
+
+    lektorfile_text = textwrap.dedent(u"""
+        [project]
+        name = Themes Project
+    """)
+    theme_project_tmpdir.join("themes.lektorproject").write_text(lektorfile_text,
+                                                                 "utf8",
+                                                                 ensure=True)
+
+    shutil.rmtree(str(theme_project_tmpdir.join('themes', 'blog_theme')))
+    project = Project.from_path(str(theme_project_tmpdir))
+    env = theme_env(project)
+    assert os.path.basename(env.theme_path) == 'project_theme'
