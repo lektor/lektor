23681001744c67f44d98b2806db69c01e70f38ad
Mickael Reuzé <>
2018-06-13 11:20:04 +0200

    PEP8 errors fixed

--- tests/test_urls.py
+++ tests/test_urls.py
@@ -29,24 +29,24 @@ def test_basic_url_to_with_alts(pad):
 
 @pytest.mark.parametrize("path, alt, absolute, external, base_url, expected", [
     ("/projects/slave/", "de", None, None, None, '../../de/projects/sklave/'),
-    ("/projects/slave/", "de", True, None, None,'/de/projects/sklave/'),
-    ("/projects/slave/", "de", True, True, None,  '/de/projects/sklave/'), #
-    ("/projects/slave/", "de", True, True, '/content/',  '/de/projects/sklave/'),#
-    ("/projects/slave/", "de", None, True, None,  '/projects/slave1/de/projects/sklave/'),
-    ("/projects/slave/", "de", None, True, '/content/',  '/projects/slave1/de/projects/sklave/'),#
-    ("/projects/slave/", "de", None, None, '/content/',  '../de/projects/sklave/'),
-    ("/projects/slave/", None, True, None, None,  '/projects/slave/'),
-    ("/projects/slave/", None, True, True, None,  '/projects/slave/'),#
-    ("/projects/slave/", None, True, True, '/content/',  '/projects/slave/'),
-    ("/projects/slave/", None, True, None, '/content/',  '/projects/slave/'),
+    ("/projects/slave/", "de", True, None, None, '/de/projects/sklave/'),
+    ("/projects/slave/", "de", True, True, None, '/de/projects/sklave/'), #
+    ("/projects/slave/", "de", True, True, '/content/', '/de/projects/sklave/'),#
+    ("/projects/slave/", "de", None, True, None, '/projects/slave1/de/projects/sklave/'),
+    ("/projects/slave/", "de", None, True, '/content/', '/projects/slave1/de/projects/sklave/'),#
+    ("/projects/slave/", "de", None, None, '/content/', '../de/projects/sklave/'),
+    ("/projects/slave/", None, True, None, None, '/projects/slave/'),
+    ("/projects/slave/", None, True, True, None, '/projects/slave/'),#
+    ("/projects/slave/", None, True, True, '/content/', '/projects/slave/'),
+    ("/projects/slave/", None, True, None, '/content/', '/projects/slave/'),
     ("/projects/slave/", None, None, True, None, '/projects/slave1/projects/slave/'),
-    ("/projects/slave/", None, None, True, '/content/',  '/projects/slave1/projects/slave/'),
-    ("/projects/slave/", None, None, None, '/content/',  '../projects/slave/'),
+    ("/projects/slave/", None, None, True, '/content/', '/projects/slave1/projects/slave/'),
+    ("/projects/slave/", None, None, None, '/content/', '../projects/slave/'),
 ])
-def test_param(pad,path,alt,absolute, external, base_url, expected):
-    if external :
+def test_param(pad, path, alt, absolute, external, base_url, expected):
+    if external:
         pad.db.config.base_url = "/projects/slave1/"
 
     wolf_en = pad.get('/projects/wolf')
 
-    assert wolf_en.url_to(path,alt,absolute, external, base_url) == expected
+    assert wolf_en.url_to(path, alt, absolute, external, base_url) == expected
