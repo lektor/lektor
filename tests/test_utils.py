c23965e9c1ace387e484ad4b1553bc2640b9f9bc
Ionuț Ciocîrlan <ionut.ciocirlan@gmail.com>
2018-06-19 15:41:37 +0300

    memoize decorator

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,3 +1,6 @@
+import logging
+
+
 def test_join_path():
     from lektor.utils import join_path
 
@@ -63,3 +66,91 @@ def test_parse_path():
     assert parse_path('/foo/bar') == ['foo', 'bar']
     assert parse_path('/foo/bar/') == ['foo', 'bar']
     assert parse_path('/foo/bar/../stuff') == ['foo', 'bar', 'stuff']
+
+
+def test_memoize(caplog):
+    from lektor.utils import memoize
+
+    @memoize
+    def func():
+        pass
+
+    with caplog.at_level(logging.DEBUG, logger='lektor'):
+        retval = func() # pylint: disable=assignment-from-no-return
+
+        assert len(caplog.records) == 1
+        assert caplog.records[0].message == "@memoize: cache miss"
+
+        caplog.clear()
+
+        assert func() == retval
+        assert len(caplog.records) == 0
+
+    @memoize
+    def func(x): # pylint: disable=function-redefined
+        return x
+
+    with caplog.at_level(logging.DEBUG, logger='lektor'):
+        retval = func(5)
+
+        assert len(caplog.records) == 1
+        assert caplog.records[0].message == "@memoize: cache miss"
+        caplog.clear()
+
+        assert func(5) == retval
+        assert len(caplog.records) == 0
+
+        retval = func('five')
+
+        assert len(caplog.records) == 1
+        assert caplog.records[0].message == "@memoize: cache miss"
+        caplog.clear()
+
+        assert func('five') == retval
+        assert len(caplog.records) == 0
+
+    @memoize
+    def func(x, y): # pylint: disable=function-redefined
+        return (x, y)
+
+    with caplog.at_level(logging.DEBUG, logger='lektor'):
+        retval = func(5, 'y')
+
+        assert len(caplog.records) == 1
+        assert caplog.records[0].message == "@memoize: cache miss"
+        caplog.clear()
+
+        assert func(5, y='y') == retval
+        assert len(caplog.records) == 0
+
+        assert func(x=5, y='y') == retval
+        assert len(caplog.records) == 0
+
+        assert func(x=5, y='w') != retval
+        assert len(caplog.records) == 1
+        assert caplog.records[0].message == "@memoize: cache miss"
+        caplog.clear()
+
+    @memoize
+    def func(x, y, z='zee'): # pylint: disable=function-redefined
+        return (x, y, z)
+
+    with caplog.at_level(logging.DEBUG, logger='lektor.runtime'):
+        retval = func(5, 'y')
+
+        assert len(caplog.records) == 1
+        assert caplog.records[0].message == "@memoize: cache miss"
+        caplog.clear()
+
+        assert func(5, 'y', 'zee') == retval
+        assert len(caplog.records) == 0
+
+        assert func(5, z='zee', y='y') == retval
+        assert len(caplog.records) == 0
+
+        assert func(x=5, y='y', z='zee') == retval
+        assert len(caplog.records) == 0
+
+        assert func(x=5, y='y', z='wee') != retval
+        assert len(caplog.records) == 1
+        caplog.clear()
