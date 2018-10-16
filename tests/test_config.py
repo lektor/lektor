fa0c42a008055e1150f2a4afd820263b02ffc5d8
jossnaz <lukas.meier@gmail.com>
2018-10-15 19:15:37 -0500

    test_config.py added test_database_cache_disabled_ondefault

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,3 +1,12 @@
+from lektor.db import DatabaseCache
+
+
 def test_custom_attachment_types(env):
     attachment_types = env.load_config().values['ATTACHMENT_TYPES']
     assert attachment_types['.foo'] == 'text'
+
+
+def test_database_cache_disabled_ondefault(env):
+    enabled = DatabaseCache.is_cache_enabled(env)
+    assert enabled is False
+    assert DatabaseCache._cache_datamodels is None
