0acbd7cebfbdb0579b8b3345f6f5780c9d8e0e7e
goanpeca <goanpeca@gmail.com>
2020-04-12 12:39:21 -0500

    Boo 3  [ci skip]

--- .github/scripts/check_js_skip.sh
+++ .github/scripts/check_js_skip.sh
@@ -1,7 +1,14 @@
 # Check if files ending in .js changed on the current PR
-result=$(git diff --name-only $GITHUB_HEAD_REF $GITHUB_BASE_REF | grep -c ".*\.jsx?")
+
+test1=$(git rev-parse origin/$GITHUB_BASE_REF)
+test2=$(git rev-parse origin/$GITHUB_HEAD_REF)
+result=$(git diff --name-only $test1 $test2)
+echo $test1
+echo $test2
+echo $result
+
 if [[ $result != '0' ]]; then 
-  echo 'Files including the filter: $result'
+  echo "Files including the filter: $result"
   echo "::set-output name=JS_SKIP::true"
 else
   echo "::set-output name=JS_SKIP::false"
