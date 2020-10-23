430a5c5acef21686751102aaedc2194932247b52
Jakob Schnitzer <mail@jakobschnitzer.de>
2020-10-23 16:46:30 +0200

    compat: remove PY2 branch
    Also remove some unused variables. Doesn't clean up any of the places
    where _compat is imported yet.

--- lektor/_compat.py
+++ lektor/_compat.py
@@ -9,52 +9,26 @@
 
     Taken from jinja2/_compat.py
 """
-# pylint: disable=invalid-name, import-error, unused-import, undefined-variable, reimported
-import sys
+# pylint: disable=unused-import
+import queue  # noqa
+from io import BytesIO  # noqa
+from io import StringIO  # noqa
 
 
-PY2 = sys.version_info[0] == 2
-_identity = lambda x: x
+PY2 = False
 
 
-if PY2:
-    unichr = unichr  # pylint: disable=self-assigning-variable  # noqa
-    text_type = unicode  # noqa
-    range_type = xrange  # noqa
-    string_types = (str, unicode)  # noqa
-    integer_types = (int, long)  # noqa
+range_type = range
+text_type = str
+string_types = (str,)
+integer_types = (int,)
 
-    iterkeys = lambda d: d.iterkeys()
-    itervalues = lambda d: d.itervalues()
-    iteritems = lambda d: d.iteritems()
-
-    from cStringIO import StringIO as BytesIO, StringIO
-    import Queue as queue  # noqa
-
-    NativeStringIO = BytesIO
-
-    exec(  # pylint: disable=exec-used
-        "def reraise(tp, value, tb=None):\n raise tp, value, tb"
-    )
+iterkeys = lambda d: iter(d.keys())
+itervalues = lambda d: iter(d.values())
+iteritems = lambda d: iter(d.items())
 
 
-else:
-    unichr = chr
-    range_type = range
-    text_type = str
-    string_types = (str,)
-    integer_types = (int,)
-
-    iterkeys = lambda d: iter(d.keys())
-    itervalues = lambda d: iter(d.values())
-    iteritems = lambda d: iter(d.items())
-
-    from io import BytesIO, StringIO
-    import queue  # noqa
-
-    NativeStringIO = StringIO
-
-    def reraise(tp, value, tb=None):
-        if value.__traceback__ is not tb:
-            raise value.with_traceback(tb)
-        raise value
+def reraise(tp, value, tb=None):
+    if value.__traceback__ is not tb:
+        raise value.with_traceback(tb)
+    raise value
