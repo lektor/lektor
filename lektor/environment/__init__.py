18c8e30cd76feaeb7fd6fbcfd6876f202e71599e
Jeff Dairiki <dairiki@dairiki.org>
2021-02-17 17:27:24 -0800

    Fix reference cycle in Environment

--- lektor/environment/__init__.py
+++ lektor/environment/__init__.py
@@ -260,7 +260,8 @@ class Environment:
         )
         return values
 
-    def select_jinja_autoescape(self, filename):
+    @staticmethod
+    def select_jinja_autoescape(filename):
         if filename is None:
             return False
         return filename.endswith((".html", ".htm", ".xml", ".xhtml"))
