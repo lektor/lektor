49ac8d604a175a4a9048927da9110bc83b072a51
Jakob Schnitzer <mail@jakobschnitzer.de>
2021-09-10 22:43:09 +0200

    ServerStatus: rewrite with hooks

--- lektor/admin/static/js/components/ServerStatus.tsx
+++ lektor/admin/static/js/components/ServerStatus.tsx
@@ -1,65 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { loadData } from "../fetch";
 import { trans } from "../i18n";
 
 type State = { serverIsUp: boolean; projectId: string | null };
 
-export default class ServerStatus extends React.Component<unknown, State> {
-  intervalId: number | null;
+type Response = { project_id: string };
 
-  constructor(props: unknown) {
-    super(props);
-    this.state = {
-      serverIsUp: true,
-      projectId: null,
-    };
+export default function ServerStatus() {
+  const [state, setState] = useState<State>({
+    serverIsUp: true,
+    projectId: null,
+  });
 
-    this.intervalId = null;
-    this.onInterval = this.onInterval.bind(this);
-  }
-
-  componentDidMount() {
-    this.intervalId = window.setInterval(this.onInterval, 2000);
-  }
-
-  componentWillUnmount() {
-    if (this.intervalId !== null) {
-      window.clearInterval(this.intervalId);
-      this.intervalId = null;
-    }
-  }
-
-  onInterval() {
-    loadData("/ping", {}).then(
-      (resp) => {
-        if (this.state.projectId === null) {
-          this.setState({
-            projectId: resp.project_id,
-          });
+  useEffect(() => {
+    const onInterval = () => {
+      loadData("/ping", {}).then(
+        ({ project_id }: Response) => {
+          setState(({ projectId }) =>
+            projectId === null
+              ? { projectId: project_id, serverIsUp: true }
+              : { projectId, serverIsUp: projectId === project_id }
+          );
+        },
+        () => {
+          setState((s) => ({ ...s, serverIsUp: false }));
         }
-        this.setState((state) => ({
-          serverIsUp: state.projectId === resp.project_id,
-        }));
-      },
-      () => {
-        this.setState({
-          serverIsUp: false,
-        });
-      }
-    );
-  }
+      );
+    };
+    const id = window.setInterval(onInterval, 2000);
+    return () => window.clearInterval(id);
+  }, []);
 
-  render() {
-    if (this.state.serverIsUp) {
-      return null;
-    }
-    return (
-      <div className="server-down-panel">
-        <div className="server-down-dialog">
-          <h3>{trans("ERROR_SERVER_UNAVAILABLE")}</h3>
-          <p>{trans("ERROR_SERVER_UNAVAILABLE_MESSAGE")}</p>
-        </div>
-      </div>
-    );
+  if (state.serverIsUp) {
+    return null;
   }
+  return (
+    <div className="server-down-panel">
+      <div className="server-down-dialog">
+        <h3>{trans("ERROR_SERVER_UNAVAILABLE")}</h3>
+        <p>{trans("ERROR_SERVER_UNAVAILABLE_MESSAGE")}</p>
+      </div>
+    </div>
+  );
 }
