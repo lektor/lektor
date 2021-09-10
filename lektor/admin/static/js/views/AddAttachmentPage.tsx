542b57e625c0ce0539c91b48fdb00bf7900dc658
Jakob Schnitzer <mail@jakobschnitzer.de>
2021-09-10 22:18:46 +0200

    AddAttachmentsPage: rewrite with hooks

--- lektor/admin/static/js/views/AddAttachmentPage.tsx
+++ lektor/admin/static/js/views/AddAttachmentPage.tsx
@@ -1,4 +1,10 @@
-import React, { Component, createRef, RefObject } from "react";
+import React, {
+  ChangeEvent,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import { RecordProps } from "../components/RecordComponent";
 import { getApiUrl } from "../utils";
 import { loadData } from "../fetch";
@@ -11,119 +17,95 @@ type NewAttachmentInfo = {
   can_upload: boolean;
 };
 
-type State = {
-  newAttachmentInfo: NewAttachmentInfo | null;
-  currentFiles: File[];
-  isUploading: boolean;
-  currentProgress: number;
-};
+function AddAttachmentPage({ record }: Pick<RecordProps, "record">) {
+  const [newAttachmentInfo, setNewAttachmentInfo] =
+    useState<NewAttachmentInfo | null>(null);
+  const fileInput = useRef<HTMLInputElement | null>(null);
+  const [currentFiles, setCurrentFiles] = useState<File[]>([]);
+  const [isUploading, setIsUploading] = useState(false);
+  const [currentProgress, setCurrentProgress] = useState(0);
+  const path = record.path;
 
-type Props = Pick<RecordProps, "record">;
+  useEffect(() => {
+    let ignore = false;
 
-class AddAttachmentPage extends Component<Props, State> {
-  fileInput: RefObject<HTMLInputElement>;
-
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      newAttachmentInfo: null,
-      currentFiles: [],
-      isUploading: false,
-      currentProgress: 0,
-    };
-    this.fileInput = createRef();
-    this.onFileSelected = this.onFileSelected.bind(this);
-  }
-
-  componentDidMount() {
-    this.syncDialog();
-  }
-
-  componentDidUpdate(prevProps: Props) {
-    if (prevProps.record.path !== this.props.record.path) {
-      this.syncDialog();
-    }
-  }
-
-  syncDialog() {
-    loadData("/newattachment", { path: this.props.record.path }).then(
-      (newAttachmentInfo: NewAttachmentInfo) => {
-        this.setState({ newAttachmentInfo });
-      },
-      showErrorDialog
-    );
-  }
-
-  onFileSelected({ target }: { target: HTMLInputElement }) {
-    if (this.state.isUploading || !target.files) {
-      return;
-    }
-
-    const files = Array.prototype.slice.call(target.files, 0);
-    this.setState({ currentFiles: files, isUploading: true });
-
-    const formData = new FormData();
-    formData.append("path", this.props.record.path ?? "");
-    files.forEach((file) => {
-      formData.append("file", file, file.name);
-    });
-
-    const xhr = new XMLHttpRequest();
-    xhr.open("POST", getApiUrl("/newattachment"));
-    xhr.onprogress = (event) => {
-      const newProgress = Math.round((event.loaded * 100) / event.total);
-      if (newProgress !== this.state.currentProgress) {
-        this.setState({ currentProgress: newProgress });
+    loadData("/newattachment", { path }).then((resp: NewAttachmentInfo) => {
+      if (!ignore) {
+        setNewAttachmentInfo(resp);
       }
+    }, showErrorDialog);
+    return () => {
+      ignore = true;
     };
-    xhr.onload = () => {
-      this.setState({ isUploading: false, currentProgress: 100 }, () => {
-        dispatch("lektor-attachments-changed", this.props.record.path);
+  }, [path]);
+
+  const onFileSelected = useCallback(
+    ({ target }: ChangeEvent<HTMLInputElement>) => {
+      if (isUploading || !target.files) {
+        return;
+      }
+
+      const files = Array.prototype.slice.call(target.files, 0);
+      setCurrentFiles(files);
+      setIsUploading(true);
+
+      const formData = new FormData();
+      formData.append("path", path);
+      files.forEach((file) => {
+        formData.append("file", file, file.name);
       });
-    };
-    xhr.send(formData);
+
+      const xhr = new XMLHttpRequest();
+      xhr.open("POST", getApiUrl("/newattachment"));
+      xhr.onprogress = (event) => {
+        setCurrentProgress(Math.round((event.loaded * 100) / event.total));
+      };
+      xhr.onload = () => {
+        setIsUploading(false);
+        setCurrentProgress(100);
+        dispatch("lektor-attachments-changed", path);
+      };
+      xhr.send(formData);
+    },
+    [path, isUploading]
+  );
+
+  if (!newAttachmentInfo) {
+    return null;
   }
 
-  render() {
-    const { newAttachmentInfo, currentFiles, currentProgress } = this.state;
-
-    if (!newAttachmentInfo) {
-      return null;
-    }
-
-    return (
-      <div>
-        <h2>{trans_format("ADD_ATTACHMENT_TO", newAttachmentInfo.label)}</h2>
-        <p>{trans("ADD_ATTACHMENT_NOTE")}</p>
-        <ul>
-          {currentFiles.map((file) => (
-            <li key={file.name}>
-              {file.name} ({file.type})
-            </li>
-          ))}
-        </ul>
-        <p>
-          {trans("PROGRESS")}: {currentProgress}%
-        </p>
-        <input
-          type="file"
-          ref={this.fileInput}
-          multiple
-          style={{ display: "none" }}
-          onChange={this.onFileSelected}
-        />
-        <div className="actions">
-          <button
-            type="button"
-            className="btn btn-primary"
-            onClick={() => this.fileInput.current?.click()}
-          >
-            {trans("UPLOAD")}
-          </button>
-        </div>
+  return (
+    <div>
+      <h2>{trans_format("ADD_ATTACHMENT_TO", newAttachmentInfo.label)}</h2>
+      <p>{trans("ADD_ATTACHMENT_NOTE")}</p>
+      <ul>
+        {currentFiles.map((file) => (
+          <li key={file.name}>
+            {file.name} ({file.type})
+          </li>
+        ))}
+      </ul>
+      <p>
+        {trans("PROGRESS")}: {currentProgress}%
+      </p>
+      <input
+        type="file"
+        ref={fileInput}
+        multiple
+        style={{ display: "none" }}
+        onChange={onFileSelected}
+      />
+      <div className="actions">
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={() => fileInput.current?.click()}
+        >
+          {trans("UPLOAD")}
+        </button>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default AddAttachmentPage;
