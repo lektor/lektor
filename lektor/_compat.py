lektor/lektor

commit e9c9b546bb742793da94b97192f5d307d8323534

Replace werkzeug.posixemulation.rename with os.replace
This function has been always internal, and has been removed from
Werkzeug 2.x.

Since 3.3, Python provides os.replace, a cross-platform atomic rename
function.

Ref: https://github.com/pallets/werkzeug/issues/1759
Ref: https://bugs.python.org/issue8828

lektor/builder.py
@@ -12,7 +12,6 @@ from contextlib import contextmanager
 from itertools import chain
 
 import click
-from werkzeug.posixemulation import rename
 
 from lektor._compat import iteritems
 from lektor._compat import PY2
@@ -929,7 +928,7 @@ class Artifact(object):
                 op(con)
 
             if self._new_artifact_file is not None:
-                rename(self._new_artifact_file, self.dst_filename)
+                os.replace(self._new_artifact_file, self.dst_filename)
                 self._new_artifact_file = None
 
             if con is not None:

lektor/utils.py
@@ -31,7 +31,6 @@ from markupsafe import Markup
 from slugify import slugify as _slugify
 from werkzeug import urls
 from werkzeug.http import http_date
-from werkzeug.posixemulation import rename
 from werkzeug.urls import url_parse
 
 from lektor._compat import (
@@ -499,7 +498,7 @@ def atomic_open(filename, mode="r"):
     else:
         f.close()
         if tmp_filename is not None:
-            rename(tmp_filename, filename)
+            os.replace(tmp_filename, filename)
 
 
 def portable_popen(cmd, *args, **kwargs):

commit 596a3a7f40f1406d306f8e6593ca46e7e70671c7

Fix the 3.2.x branch under python 2.7
Running under python 2.7 was broken by e9c9b546b

lektor/_compat.py
@@ -37,6 +37,8 @@ if PY2:
         "def reraise(tp, value, tb=None):\n raise tp, value, tb"
     )
 
+    from werkzeug.posixemulation import rename as os_replace  # noqa
+
 
 else:
     unichr = chr
@@ -58,6 +60,8 @@ else:
         if value.__traceback__ is not tb:
             raise value.with_traceback(tb)
         raise value
+
+    from os import replace as os_replace  # noqa
 
 
 def python_2_unicode_compatible(klass):

lektor/builder.py
@@ -14,6 +14,7 @@ from itertools import chain
 import click
 
 from lektor._compat import iteritems
+from lektor._compat import os_replace
 from lektor._compat import PY2
 from lektor._compat import text_type
 from lektor.build_programs import builtin_build_programs
@@ -928,7 +929,7 @@ class Artifact(object):
                 op(con)
 
             if self._new_artifact_file is not None:
-                os.replace(self._new_artifact_file, self.dst_filename)
+                os_replace(self._new_artifact_file, self.dst_filename)
                 self._new_artifact_file = None
 
             if con is not None:

lektor/utils.py
@@ -41,6 +41,7 @@ from lektor._compat import (
     string_types,
     text_type,
     range_type,
+    os_replace,
 )
 from lektor.uilink import BUNDLE_BIN_PATH, EXTRA_PATHS
 
@@ -498,7 +499,7 @@ def atomic_open(filename, mode="r"):
     else:
         f.close()
         if tmp_filename is not None:
-            os.replace(tmp_filename, filename)
+            os_replace(tmp_filename, filename)
 
 
 def portable_popen(cmd, *args, **kwargs):