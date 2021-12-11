1e1db44147511253db351258a3c55f88de666705
Jeff Dairiki <dairiki@dairiki.org>
2021-12-11 12:25:43 -0800

    Back to dev

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ tests_require = [
 
 setup(
     name="Lektor",
-    version="3.2.3",
+    version="3.2.4.dev0",
     url="http://github.com/lektor/lektor/",
     description="A static content management system.",
     long_description=readme,
