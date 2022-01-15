lektor/lektor

commit 81cccdffdd8343ffc5970b61b262f55d7d8761bb

flow: split up into components, better memoization, use hooks

lektor/admin/static/js/widgets/flow/FlowWidget.tsx
@@ -1,0 +1,276 @@
+import React, { SetStateAction, useCallback } from "react";
+import { trans, Translatable } from "../../i18n";
+import { tokenize, serialize } from "./metaformat";
+import { Field, BaseWidgetType, WidgetProps } from "../types";
+import {
+  getWidgetComponent,
+  getWidgetComponentWithFallback,
+  FieldBox,
+} from "../../widgets";
+import FlowBlock from "./FlowBlock";
+import AddFlowBlockButtons from "./AddFlowBlockButtons";
+
+type Block = [name: string, lines: string[]];
+
+/**
+ * Parse the row string from a Lektor file for the flow format
+ * to its separate flow blocks. Each flow block can then be
+ * deserialised with deserializeFlowBlock.
+ */
+export function parseFlowFormat(value = ""): Block[] {
+  const blocks: Block[] = [];
+  const lines = value.split(/\r?\n/);
+  let blockName: string | null = null;
+  let blockLines: string[] = [];
+
+  const flush = () => {
+    if (blockName !== null) {
+      blocks.push([blockName, blockLines]);
+      blockLines = [];
+    }
+  };
+
+  for (const line of lines) {
+    // leading whitespace is ignored.
+    if (blockName === null && line.match(/^\s*$/)) {
+      continue;
+    }
+
+    const blockStart = line.match(/^####\s*([^#]*?)\s*####\s*$/);
+    if (!blockStart) {
+      if (blockName === null) {
+        // bad format :(
+        return [];
+      }
+    } else {
+      flush();
+      blockName = blockStart[1];
+      continue;
+    }
+
+    blockLines.push(line.replace(/^#####(.*?)#####$/, "####$1####"));
+  }
+
+  flush();
+  return blocks;
+}
+
+/** Serialise an array of single blocks to a string. */
+export function serializeFlowFormat(blocks: Block[]): string {
+  const serialisedBlocks: string[] = [];
+  blocks.forEach(([blockName, lines]) => {
+    serialisedBlocks.push(`#### ${blockName} ####\n`);
+    lines.forEach((line) => {
+      serialisedBlocks.push(line.replace(/^(####(.*)####)(\r?\n)?$/, "#$1#$3"));
+    });
+  });
+
+  const rv = serialisedBlocks.join("");
+
+  /* we need to chop of the last newline if it exists because this would
+     otherwise add a newline to the last block.  This is just a side effect
+     of how we serialize the meta format internally */
+  return rv[rv.length - 1] === "\n" ? rv.substr(0, rv.length - 1) : rv;
+}
+
+export interface FlowBlockModel {
+  order: number;
+  id: string;
+  fields: Field[];
+  name: string;
+  name_i18n: Translatable;
+  button_label?: string;
+}
+
+export interface FlowBlockWidgetType extends BaseWidgetType {
+  widget: "flow";
+  flowblock_order: string[];
+  flowblocks: Record<string, FlowBlockModel>;
+}
+
+export interface FlowBlockData {
+  /** The current data for this flow block. */
+  data: Record<string, string>;
+  /** The data model of the flow block. */
+  model: FlowBlockModel;
+  /** A key to identify this item to React (needed when moving them up/down). */
+  localId: number;
+}
+
+function deserializeFlowBlock(
+  model: FlowBlockModel,
+  lines: string[],
+  localId: number
+): FlowBlockData {
+  const data: Record<string, string> = {};
+  const rawData: Record<string, string> = {};
+
+  tokenize(lines).forEach(([key, lines]) => {
+    rawData[key] = lines.join("");
+  });
+
+  model.fields.forEach((field) => {
+    let value = rawData[field.name] || "";
+    const Widget = getWidgetComponent(field.type);
+    if (!value && field.default) {
+      value = field.default;
+    }
+    if (Widget && Widget.deserializeValue) {
+      value = Widget.deserializeValue(value, field.type);
+    }
+    data[field.name] = value;
+  });
+
+  return { localId, model, data };
+}
+
+function serializeFlowBlock(
+  flockBlockModel: FlowBlockModel,
+  data: Record<string, string>
+) {
+  const rv: [string, string][] = [];
+  flockBlockModel.fields.forEach((field) => {
+    const Widget = getWidgetComponent(field.type);
+    if (Widget === null) {
+      return;
+    }
+
+    let value = data[field.name];
+    if (value === undefined || value === null) {
+      return;
+    }
+
+    if (Widget.serializeValue) {
+      value = Widget.serializeValue(value, field.type);
+    }
+    rv.push([field.name, value]);
+  });
+  return serialize(rv);
+}
+
+export function FlowWidget(
+  props: WidgetProps<readonly FlowBlockData[], FlowBlockWidgetType>
+) {
+  const { value, onChange } = props;
+
+  const moveBlock = useCallback(
+    (idx: number, offset: number) => {
+      onChange((prevValue) => {
+        const newIndex = idx + offset;
+        if (newIndex < 0 || newIndex >= prevValue.length) {
+          return prevValue;
+        }
+
+        const newValue = [...prevValue];
+        newValue[newIndex] = prevValue[idx];
+        newValue[idx] = prevValue[newIndex];
+        return newValue;
+      });
+    },
+    [onChange]
+  );
+
+  const removeBlock = useCallback(
+    (idx: number) => {
+      if (confirm(trans("REMOVE_FLOWBLOCK_PROMPT"))) {
+        onChange((v) => v.filter((item, i) => i !== idx));
+      }
+    },
+    [onChange]
+  );
+
+  const addNewBlock = useCallback(
+    (flowBlockModel: FlowBlockModel) => {
+      onChange((prevValue) => {
+        // find the first available id for this new block - use findMax + 1
+        const newBlockId =
+          Math.max(0, ...prevValue.map(({ localId }) => localId)) + 1;
+        // this is a rather ugly way to do this, but hey, it works.
+        const newBlock = deserializeFlowBlock(flowBlockModel, [], newBlockId);
+        return [...prevValue, newBlock];
+      });
+    },
+    [onChange]
+  );
+
+  const renderFormField = useCallback(
+    (block: FlowBlockData, field: Field) => {
+      const fieldValue = block.data[field.name];
+      let placeholder = field.default;
+      const Widget = getWidgetComponentWithFallback(field.type);
+      if (Widget.deserializeValue && placeholder != null) {
+        placeholder = Widget.deserializeValue(placeholder, field.type);
+      }
+
+      const setFieldValue = (fieldName: string, v: SetStateAction<string>) => {
+        onChange((prevValue) => {
+          const newValue = [...prevValue];
+          const idx = newValue.indexOf(block);
+          const data = {
+            ...block.data,
+            [fieldName]: typeof v === "function" ? v(block.data[fieldName]) : v,
+          };
+          newValue[idx] = { ...block, data };
+          return newValue;
+        });
+      };
+
+      return (
+        <FieldBox
+          key={field.name}
+          value={fieldValue}
+          placeholder={placeholder}
+          field={field}
+          setFieldValue={setFieldValue}
+        />
+      );
+    },
+    [onChange]
+  );
+
+  const { flowblock_order, flowblocks } = props.type;
+  return (
+    <div className="flow">
+      {value.map((block, idx) => (
+        <FlowBlock
+          key={block.localId}
+          block={block}
+          moveBlock={moveBlock}
+          renderFormField={renderFormField}
+          removeBlock={removeBlock}
+          length={value.length}
+          idx={idx}
+        />
+      ))}
+      <AddFlowBlockButtons
+        flowblock_order={flowblock_order}
+        flowblocks={flowblocks}
+        addBlock={addNewBlock}
+      />
+    </div>
+  );
+}
+
+FlowWidget.deserializeValue = (
+  value: string,
+  { flowblocks }: FlowBlockWidgetType
+): FlowBlockData[] => {
+  let blockId = 0;
+  const blocks: FlowBlockData[] = [];
+  parseFlowFormat(value).forEach(([id, lines]) => {
+    const flowBlock = flowblocks[id];
+    if (flowBlock !== undefined) {
+      blocks.push(deserializeFlowBlock(flowBlock, lines, ++blockId));
+    }
+  });
+  return blocks;
+};
+
+FlowWidget.serializeValue = (value: FlowBlockData[]) => {
+  return serializeFlowFormat(
+    value.map(({ model: flowBlockModel, data }) => [
+      flowBlockModel.id,
+      serializeFlowBlock(flowBlockModel, data),
+    ])
+  );
+};

lektor/admin/static/js/widgets/FlowWidget.tsx
@@ -1,350 +1,0 @@
-import React from "react";
-import { trans, Translatable, trans_obj } from "../i18n";
-import { tokenize, serialize } from "../metaformat";
-import { formatUserLabel } from "../userLabel";
-import { Field, BaseWidgetType, WidgetProps } from "./types";
-import {
-  getWidgetComponent,
-  getWidgetComponentWithFallback,
-  FieldBox,
-  FieldRows,
-} from "../widgets";
-
-type Block = [string, string[]];
-
-export function parseFlowFormat(value: string): Block[] | null {
-  const blocks: Block[] = [];
-  let buf = [];
-  const lines = value.split(/\r?\n/);
-  let block = null;
-
-  for (const line of lines) {
-    // leading whitespace is ignored.
-    if (block === null && line.match(/^\s*$/)) {
-      continue;
-    }
-
-    const blockStart = line.match(/^####\s*([^#]*?)\s*####\s*$/);
-    if (!blockStart) {
-      if (block === null) {
-        // bad format :(
-        return null;
-      }
-    } else {
-      if (block !== null) {
-        blocks.push([block, buf]);
-        buf = [];
-      }
-      block = blockStart[1];
-      continue;
-    }
-
-    buf.push(line.replace(/^#####(.*?)#####$/, "####$1####"));
-  }
-
-  if (block !== null) {
-    blocks.push([block, buf]);
-  }
-
-  return blocks;
-}
-
-export function serializeFlowFormat(blocks: Block[]): string {
-  const serialisedBlocks: string[] = [];
-  blocks.forEach(([blockName, lines]) => {
-    serialisedBlocks.push("#### " + blockName + " ####\n");
-    lines.forEach((line) => {
-      serialisedBlocks.push(line.replace(/^(####(.*)####)(\r?\n)?$/, "#$1#$3"));
-    });
-  });
-
-  const rv = serialisedBlocks.join("");
-
-  /* we need to chop of the last newline if it exists because this would
-     otherwise add a newline to the last block.  This is just a side effect
-     of how we serialize the meta format internally */
-  return rv[rv.length - 1] === "\n" ? rv.substr(0, rv.length - 1) : rv;
-}
-
-interface FlowBlockModel {
-  order: number;
-  id: string;
-  fields: Field[];
-  name: string;
-  name_i18n: Translatable;
-  button_label?: string;
-}
-
-export interface FlowBlockWidgetType extends BaseWidgetType {
-  widget: "flow";
-  flowblock_order: string[];
-  flowblocks: Record<string, FlowBlockModel>;
-}
-
-interface FlowBlock {
-  data: Record<string, unknown>;
-  flowBlockModel: FlowBlockModel;
-  localId: number;
-  collapsed: boolean;
-}
-
-function deserializeFlowBlock(
-  flowBlockModel: FlowBlockModel,
-  lines: string[],
-  localId: number
-): FlowBlock {
-  const data: Record<string, unknown> = {};
-  const rawData: Record<string, string> = {};
-
-  tokenize(lines).forEach((item) => {
-    const [key, lines] = item;
-    const value = lines.join("");
-    rawData[key] = value;
-  });
-
-  flowBlockModel.fields.forEach((field) => {
-    let value = rawData[field.name] || "";
-    const Widget = getWidgetComponent(field.type);
-    if (!value && field.default) {
-      value = field.default;
-    }
-    if (Widget && Widget.deserializeValue) {
-      value = Widget.deserializeValue(value, field.type);
-    }
-    data[field.name] = value;
-  });
-
-  return { localId, flowBlockModel, data, collapsed: false };
-}
-
-function serializeFlowBlock(
-  flockBlockModel: FlowBlockModel,
-  data: Record<string, unknown>
-) {
-  const rv: [string, string][] = [];
-  flockBlockModel.fields.forEach((field) => {
-    const Widget = getWidgetComponent(field.type);
-    if (Widget === null) {
-      return;
-    }
-
-    let value = data[field.name];
-    if (value === undefined || value === null) {
-      return;
-    }
-
-    if (Widget.serializeValue) {
-      // @ts-expect-error The data record is not properly typed yet
-      value = Widget.serializeValue(value, field.type);
-    }
-
-    // @ts-expect-error The data record is not properly typed yet
-    rv.push([field.name, value]);
-  });
-  return serialize(rv);
-}
-
-export class FlowWidget extends React.PureComponent<
-  WidgetProps<FlowBlock[], FlowBlockWidgetType>
-> {
-  static deserializeValue(
-    value: string,
-    type: FlowBlockWidgetType
-  ): (FlowBlock | null)[] {
-    let blockId = 0;
-    return (parseFlowFormat(value) ?? []).map((item) => {
-      const [id, lines] = item;
-      const flowBlock = type.flowblocks[id];
-      if (flowBlock !== undefined) {
-        return deserializeFlowBlock(flowBlock, lines, ++blockId);
-      }
-      return null;
-    });
-  }
-
-  static serializeValue(value: FlowBlock[]) {
-    return serializeFlowFormat(
-      value.map((item) => {
-        return [
-          item.flowBlockModel.id,
-          serializeFlowBlock(item.flowBlockModel, item.data),
-        ];
-      })
-    );
-  }
-
-  moveBlock(idx: number, offset: number) {
-    if (this.props.value) {
-      const newIndex = idx + offset;
-      if (newIndex < 0 || newIndex >= this.props.value.length) {
-        return;
-      }
-
-      const newValue = [...this.props.value];
-      newValue[newIndex] = this.props.value[idx];
-      newValue[idx] = this.props.value[newIndex];
-
-      this.props.onChange(newValue);
-    }
-  }
-
-  removeBlock(idx: number) {
-    if (this.props.value) {
-      if (confirm(trans("REMOVE_FLOWBLOCK_PROMPT"))) {
-        this.props.onChange(this.props.value.filter((item, i) => i !== idx));
-      }
-    }
-  }
-
-  addNewBlock(key: string) {
-    if (this.props.value) {
-      const flowBlockModel = this.props.type.flowblocks[key];
-
-      // find the first available id for this new block - use findMax + 1
-      const blockIds = this.props.value.map((block) => block.localId);
-      const newBlockId = blockIds.length === 0 ? 1 : Math.max(...blockIds) + 1;
-
-      // this is a rather ugly way to do this, but hey, it works.
-      const newValue = [
-        ...this.props.value,
-        deserializeFlowBlock(flowBlockModel, [], newBlockId),
-      ];
-      this.props.onChange(newValue);
-    }
-  }
-
-  toggleBlock(idx: number) {
-    if (this.props.value) {
-      const { collapsed } = this.props.value[idx];
-      const newValue = [...this.props.value];
-      newValue[idx] = { ...this.props.value[idx], collapsed: !collapsed };
-      this.props.onChange(newValue, true); // true => just ui changed
-    }
-  }
-
-  renderFormField(blockInfo: FlowBlock, field: Field, idx: number) {
-    if (this.props.value) {
-      // @ts-expect-error The data record is not properly typed yet
-      const value: string = blockInfo.data[field.name];
-      let placeholder = field.default;
-      const Widget = getWidgetComponentWithFallback(field.type);
-      if (Widget.deserializeValue && placeholder != null) {
-        placeholder = Widget.deserializeValue(placeholder, field.type);
-      }
-
-      const setFieldValue = (field: Field, value: unknown) => {
-        blockInfo.data[field.name] = value;
-
-        // @ts-expect-error The data record is not properly typed yet
-        this.props.onChange([...this.props.value]);
-      };
-
-      return (
-        <FieldBox
-          key={idx}
-          value={value}
-          placeholder={placeholder}
-          field={field}
-          setFieldValue={setFieldValue}
-        />
-      );
-    }
-  }
-
-  renderBlocks() {
-    const flowBlocks = this.props.value ?? [];
-    return flowBlocks.map((blockInfo, idx) => {
-      // bad block is no block
-      if (blockInfo === null) {
-        return null;
-      }
-
-      const fields = blockInfo.collapsed ? null : (
-        <FieldRows
-          fields={blockInfo.flowBlockModel.fields}
-          renderFunc={this.renderFormField.bind(this, blockInfo)}
-        />
-      );
-      const buttonClass = "btn btn-secondary btn-sm border";
-
-      return (
-        <div key={blockInfo.localId} className="flow-block">
-          <div className="btn-group action-bar">
-            <button
-              type="button"
-              className={buttonClass}
-              title={blockInfo.collapsed ? trans("EXPAND") : trans("COLLAPSE")}
-              onClick={this.toggleBlock.bind(this, idx)}
-            >
-              <i
-                className={
-                  blockInfo.collapsed ? "fa fa-expand" : "fa fa-compress"
-                }
-              />
-            </button>
-            <button
-              type="button"
-              className={buttonClass}
-              title={trans("UP")}
-              disabled={idx === 0}
-              onClick={this.moveBlock.bind(this, idx, -1)}
-            >
-              <i className="fa fa-fw fa-chevron-up" />
-            </button>
-            <button
-              type="button"
-              className={buttonClass}
-              title={trans("DOWN")}
-              disabled={idx >= flowBlocks.length - 1}
-              onClick={this.moveBlock.bind(this, idx, 1)}
-            >
-              <i className="fa fa-fw fa-chevron-down" />
-            </button>
-            <button
-              type="button"
-              className={buttonClass}
-              title={trans("REMOVE")}
-              onClick={this.removeBlock.bind(this, idx)}
-            >
-              <i className="fa fa-fw fa-times" />
-            </button>
-          </div>
-          <h4 className="block-name">
-            {formatUserLabel(blockInfo.flowBlockModel.name_i18n)}
-          </h4>
-          {fields}
-        </div>
-      );
-    });
-  }
-
-  render() {
-    const addBlockButtons = this.props.type.flowblock_order.map((key) => {
-      const flowBlockModel = this.props.type.flowblocks[key];
-      const label = flowBlockModel.button_label
-        ? formatUserLabel(flowBlockModel.button_label)
-        : formatUserLabel(flowBlockModel.name_i18n);
-      return (
-        <button
-          type="button"
-          className="btn btn-secondary border"
-          onClick={this.addNewBlock.bind(this, key)}
-          title={trans_obj(flowBlockModel.name_i18n)}
-          key={flowBlockModel.id}
-        >
-          {label}
-        </button>
-      );
-    });
-
-    return (
-      <div className="flow">
-        {this.renderBlocks()}
-        <div className="add-flow-block">
-          <label>{trans("ADD_FLOWBLOCK") + ": "}</label>
-          <div className="btn-group">{addBlockButtons}</div>
-        </div>
-      </div>
-    );
-  }
-}

lektor/admin/static/js/views/add-child-page/PrimaryFieldRow.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { SetStateAction } from "react";
 import { formatUserLabel } from "../../userLabel";
 import { getWidgetComponentWithFallback } from "../../widgets";
 import { Field } from "../../widgets/types";
@@ -12,7 +12,7 @@ export default function PrimaryField({
   setPrimary,
 }: {
   primary: string;
-  setPrimary: (s: string) => void;
+  setPrimary: (s: SetStateAction<string>) => void;
   field: Field;
 }): JSX.Element {
   const Widget = getWidgetComponentWithFallback(field.type);

lektor/admin/static/js/views/add-child-page/Slug.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { SetStateAction } from "react";
 import { trans } from "../../i18n";
 import { formatUserLabel } from "../../userLabel";
 import { SlugInputWidget } from "../../widgets/SlugInputWidget";
@@ -13,7 +13,7 @@ export default function Slug({
 }: {
   id: string;
   placeholder: string;
-  setId: (s: string) => void;
+  setId: (s: SetStateAction<string>) => void;
 }): JSX.Element {
   return (
     <div className="row field-row">

lektor/admin/static/js/views/edit/EditPage.tsx
@@ -1,5 +1,6 @@
 import React, {
   FormEvent,
+  SetStateAction,
   useCallback,
   useEffect,
   useRef,
@@ -197,9 +198,12 @@ function EditPage({ record }: Pick<RecordProps, "record">) {
   }, []);
 
   const setFieldValue = useCallback(
-    (field: Field, value: string, uiChange = false) => {
-      setRecordData((r) => ({ ...r, [field.name]: value || "" }));
-      setHasPendingChanges((has) => !uiChange || has);
+    (fieldName: string, value: SetStateAction<string>) => {
+      setRecordData((r) => ({
+        ...r,
+        [fieldName]: typeof value === "function" ? value(r[fieldName]) : value,
+      }));
+      setHasPendingChanges(true);
     },
     []
   );

lektor/admin/static/js/widgets.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, SetStateAction, useCallback, useMemo } from "react";
 import { IntegerInputWidget } from "./widgets/IntegerInputWidget";
 import { MultiLineTextInputWidget } from "./widgets/MultiLineTextInputWidget";
 import { BooleanInputWidget } from "./widgets/BooleanInputWidget";
@@ -9,7 +9,7 @@ import { SlugInputWidget } from "./widgets/SlugInputWidget";
 import { SingleLineTextInputWidget } from "./widgets/SingleLineTextInputWidget";
 import { CheckboxesInputWidget } from "./widgets/CheckboxesInputWidget";
 import { SelectInputWidget } from "./widgets/SelectInputWidget";
-import { FlowWidget } from "./widgets/FlowWidget";
+import { FlowWidget } from "./widgets/flow/FlowWidget";
 import {
   LineWidget,
   SpacingWidget,
@@ -64,11 +64,14 @@ export const FieldBox = React.memo(function FieldBox(props: {
   value: string;
   placeholder: string | null;
   disabled?: boolean;
-  setFieldValue: (field: Field, value: string, uiChange?: boolean) => void;
+  setFieldValue: (fieldName: string, value: SetStateAction<string>) => void;
 }) {
-  const { field, value, placeholder, disabled } = props;
-  const onChange = (value: string, uiChange?: boolean) =>
-    props.setFieldValue(field, value, uiChange);
+  const { field, value, placeholder, disabled, setFieldValue } = props;
+
+  const onChange = useCallback(
+    (value: SetStateAction<string>) => setFieldValue(field.name, value),
+    [field, setFieldValue]
+  );
   const className = `col-md-${getFieldColumns(field)}`;
 
   const Widget = getWidgetComponentWithFallback(field.type);
@@ -129,8 +132,8 @@ export function getFieldColumns(field: { type: Pick<WidgetType, "width"> }) {
 /**
  * Process fields into rows.
  */
-function processFields(fields: Field[]) {
-  const rows = [];
+function processFields(fields: Field[]): Field[][] {
+  const rows: Field[][] = [];
   let currentColumns = 0;
   let row: Field[] = [];
 
@@ -177,12 +180,13 @@ export function FieldRows({
   renderFunc,
 }: {
   fields: Field[];
-  renderFunc: (field: Field, index: number) => ReactNode;
+  renderFunc: (field: Field) => ReactNode;
 }) {
+  const rows = useMemo(() => processFields(fields), [fields]);
   return (
     <>
-      {processFields(fields).map((row, idx) => (
-        <div className="row field-row" key={"normal-" + idx}>
+      {rows.map((row, idx) => (
+        <div key={idx} className="row field-row">
           {row.map(renderFunc)}
         </div>
       ))}

lektor/admin/static/js/widgets/flow/AddFlowBlockButtons.tsx
@@ -1,0 +1,39 @@
+import React from "react";
+import { trans, trans_obj } from "../../i18n";
+import { formatUserLabel } from "../../userLabel";
+import { FlowBlockModel } from "./FlowWidget";
+
+export default function AddFlowBlockButtons({
+  flowblock_order,
+  flowblocks,
+  addBlock,
+}: {
+  flowblocks: Record<string, FlowBlockModel>;
+  flowblock_order: string[];
+  addBlock: (model: FlowBlockModel) => void;
+}): JSX.Element {
+  const models = flowblock_order.map((key) => flowblocks[key]);
+  return (
+    <div className="add-flow-block">
+      <label>{`${trans("ADD_FLOWBLOCK")}: `}</label>
+      <div className="btn-group">
+        {models.map((model) => {
+          const label = model.button_label
+            ? formatUserLabel(model.button_label)
+            : formatUserLabel(model.name_i18n);
+          return (
+            <button
+              type="button"
+              className="btn btn-secondary border"
+              onClick={() => addBlock(model)}
+              title={trans_obj(model.name_i18n)}
+              key={model.id}
+            >
+              {label}
+            </button>
+          );
+        })}
+      </div>
+    </div>
+  );
+}

lektor/admin/static/js/widgets/flow/FlowBlock.tsx
@@ -1,0 +1,75 @@
+import React, { memo, useReducer } from "react";
+import { trans } from "../../i18n";
+import { formatUserLabel } from "../../userLabel";
+import { Field } from "../types";
+import { FieldRows } from "../../widgets";
+import { FlowBlockData } from "./FlowWidget";
+
+/**
+ * Render a single flow block in the flow widget. The function to render
+ * the child input widgets is passed down.
+ */
+export default memo(function FlowBlock({
+  block,
+  moveBlock,
+  renderFormField,
+  removeBlock,
+  idx,
+  length,
+}: {
+  block: FlowBlockData;
+  moveBlock: (i: number, s: number) => void;
+  renderFormField: (b: FlowBlockData, f: Field) => JSX.Element;
+  removeBlock: (i: number) => void;
+  idx: number;
+  length: number;
+}) {
+  const [collapsed, toggle] = useReducer((s) => !s, false);
+  return (
+    <div className="flow-block">
+      <div className="btn-group action-bar">
+        <button
+          type="button"
+          className="btn btn-secondary btn-sm border"
+          title={collapsed ? trans("EXPAND") : trans("COLLAPSE")}
+          onClick={toggle}
+        >
+          <i className={collapsed ? "fa fa-expand" : "fa fa-compress"} />
+        </button>
+        <button
+          type="button"
+          className="btn btn-secondary btn-sm border"
+          title={trans("UP")}
+          disabled={idx === 0}
+          onClick={() => moveBlock(idx, -1)}
+        >
+          <i className="fa fa-fw fa-chevron-up" />
+        </button>
+        <button
+          type="button"
+          className="btn btn-secondary btn-sm border"
+          title={trans("DOWN")}
+          disabled={idx === length - 1}
+          onClick={() => moveBlock(idx, 1)}
+        >
+          <i className="fa fa-fw fa-chevron-down" />
+        </button>
+        <button
+          type="button"
+          className="btn btn-secondary btn-sm border"
+          title={trans("REMOVE")}
+          onClick={() => removeBlock(idx)}
+        >
+          <i className="fa fa-fw fa-times" />
+        </button>
+      </div>
+      <h4 className="block-name">{formatUserLabel(block.model.name_i18n)}</h4>
+      {collapsed ? null : (
+        <FieldRows
+          fields={block.model.fields}
+          renderFunc={renderFormField.bind(undefined, block)}
+        />
+      )}
+    </div>
+  );
+});

commit a88b90e60d964c8d81dbd92042374bdfdb7dc50e

Fix the ability to add an initial flowblock to a page using the admin GUI
This was broken in 81cccdf (PR #977)

lektor/admin/static/js/widgets/flow/FlowWidget.tsx
@@ -183,6 +183,7 @@ export function FlowWidget(
     (flowBlockModel: FlowBlockModel) => {
       onChange((prevValue) => {
         // find the first available id for this new block - use findMax + 1
+        prevValue ??= [];
         const newBlockId =
           Math.max(0, ...prevValue.map(({ localId }) => localId)) + 1;
         // this is a rather ugly way to do this, but hey, it works.