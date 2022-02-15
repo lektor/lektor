2dd3d3c3402153149ff7fb9ad294825eca9c087c
Jeff Dairiki <dairiki@dairiki.org>
2022-02-14 19:02:28 -0800

    Skip test when git unavailable

--- tests/test_quickstart.py
+++ tests/test_quickstart.py
@@ -1,10 +1,14 @@
+import pytest
+
 from lektor.quickstart import get_default_author
 from lektor.quickstart import get_default_author_email
+from lektor.utils import locate_executable
 
 
 def test_default_author(os_user):
     assert get_default_author() == "Lektor Test"
 
 
+@pytest.mark.skipif(locate_executable("git") is None, reason="git not installed")
 def test_default_author_email():
     assert isinstance(get_default_author_email(), str)
