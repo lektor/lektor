ed9fc32f07db3c8612f0d00641c5dacfa3dea9cd
Jeff Dairiki <dairiki@dairiki.org>
2022-09-20 19:53:51 -0700

    lektor.devtools.DevTools test coverage

--- tests/test_devserver.py
+++ tests/test_devserver.py
@@ -1,3 +1,4 @@
+import re
 import shutil
 
 import pytest
@@ -10,5 +11,39 @@ def test_DevTools(env):
     devtools = DevTools(env)
     try:
         devtools.start()
+        # check that starting again is ignored
+        watcher = devtools.watcher
+        devtools.start()
+        assert devtools.watcher is watcher
     finally:
         devtools.stop()
+        # check that stop is idempotent
+        devtools.stop()
+
+
+def test_DevTools_no_frontend_src(env, monkeypatch, tmp_path):
+    monkeypatch.setattr(DevTools, "frontend_path", tmp_path / "missing")
+    devtools = DevTools(env)
+    devtools.start()
+    assert devtools.watcher is None
+
+
+def test_DevTools_no_npm(env, monkeypatch, capsys, tmp_path):
+    monkeypatch.setattr(DevTools, "frontend_path", tmp_path)
+    monkeypatch.setattr(shutil, "which", lambda _: None)
+    devtools = DevTools(env)
+    devtools.start()
+    assert devtools.watcher is None
+    std = capsys.readouterr()
+    assert re.search(r"Can not find .* command", std.out)
+
+
+def test_DevTools_npm_install_failure(env, mocker, capsys, tmp_path):
+    mocker.patch.object(DevTools, "frontend_path", tmp_path)
+    mocker.patch("shutil.which").return_value = "npm"
+    mocker.patch("subprocess.run").return_value.returncode = 1
+    devtools = DevTools(env)
+    devtools.start()
+    assert devtools.watcher is None
+    std = capsys.readouterr()
+    assert re.search(r"Command .npm install. failed", std.out)
