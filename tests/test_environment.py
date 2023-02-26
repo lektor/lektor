81dd3f48fb5d6dd1350f4be54131f025f3d7f575
Jeff Dairiki <dairiki@dairiki.org>
2023-02-25 17:42:16 -0800

    Fix template global markdown filter (#1102)
    * refactor(markdown): allow rendering markdown without a current record
    
    Though that if there is no current record, it will not be possible to
    perform record-relative resolution of URLs for links and images.
    
    * fix: markdown jinja filter (broken in PR #992)
    
    This addresses #1100.

--- tests/test_environment.py
+++ tests/test_environment.py
@@ -1,8 +1,21 @@
+import re
 import sys
 from html import unescape
 from pathlib import Path
 
 import pytest
+
+import lektor.context
+
+
+@pytest.fixture
+def scratch_project_data(scratch_project_data):
+    # Add a sub-page to the scratch project
+    data = {"_model": "page", "title": "Subpage", "body": "Subpage body"}
+    subpage_lr = scratch_project_data / "content/sub-page/contents.lr"
+    subpage_lr.parent.mkdir()
+    subpage_lr.write_text("".join(lektor.metaformat.serialize(data.items())))
+    return scratch_project_data
 
 
 @pytest.fixture
@@ -14,6 +27,21 @@ def compile_template(scratch_env):
         return scratch_env.jinja_env.get_template(name)
 
     return compile_template
+
+
+@pytest.fixture
+def source_path():
+    return "/"
+
+
+@pytest.fixture
+def bogus_context(scratch_pad, source_path):
+    # Construct a Context that has a source, without going through all
+    # all the steps necessary to construct an Artifact.
+    with lektor.context.Context(pad=scratch_pad) as ctx:
+        if source_path is not None:
+            ctx.source = scratch_pad.get(source_path)
+        yield
 
 
 def test_jinja2_feature_autoescape(compile_template):
@@ -33,6 +61,48 @@ def test_jinja2_feature_do(compile_template):
         "{% set x = ['a'] %}{% do x.append('b') %}{{ x|join('-') }}"
     )
     assert tmpl.render() == "a-b"
+
+
+@pytest.mark.parametrize("source_path", [None, "/"])
+@pytest.mark.usefixtures("bogus_context")
+def test_jinja2_markdown_filter(compile_template):
+    tmpl = compile_template("{{ '**word**' | markdown }}")
+    assert "<strong>word</strong>" in tmpl.render()
+
+
+@pytest.mark.usefixtures("bogus_context")
+def test_jinja2_markdown_filter_resolve_links(compile_template):
+    tmpl = compile_template(
+        "{{ '[subpage](sub-page)' | markdown(resolve_links='always') }}"
+    )
+    assert re.search(r"<a.*\bhref=(['\"])sub-page/\1.*>subpage</a>", tmpl.render())
+
+
+@pytest.mark.parametrize(
+    "source_path, resolve_links",
+    [
+        (None, "if-possible"),
+        (None, "never"),
+        ("/", "never"),
+    ],
+)
+@pytest.mark.usefixtures("bogus_context")
+def test_jinja2_markdown_filter_noresolve_links(compile_template, resolve_links):
+    tmpl = compile_template(
+        f"{{{{ '[subpage](sub-page)' | markdown(resolve_links={resolve_links!r}) }}}}"
+    )
+    assert re.search(r"<a.*\bhref=(['\"])sub-page\1.*>subpage</a>", tmpl.render())
+
+
+@pytest.mark.parametrize("source_path", [None])
+@pytest.mark.usefixtures("bogus_context")
+def test_jinja2_markdown_filter_resolve_raises_if_no_source_obj(compile_template):
+    tmpl = compile_template(
+        "{{ '[subpage](sub-page)' | markdown(resolve_links='always') }}"
+    )
+    with pytest.raises(RuntimeError) as exc_info:
+        tmpl.render()
+    assert re.search(r"\bsource object\b.*\brequired\b", str(exc_info.value))
 
 
 def test_no_reference_cycle_in_environment(project):
