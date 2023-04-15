6c0f138ce81969cbae60f06a470e81bcbed348ca
Jakob Schnitzer <mail@jakobschnitzer.de>
2023-04-15 13:03:58 +0200

    frontend: npm update

--- frontend/js/context/page-context.ts
+++ frontend/js/context/page-context.ts
@@ -7,7 +7,7 @@ export const PAGE_NAMES = [
   "add-child",
   "upload",
 ] as const;
-export type PageName = typeof PAGE_NAMES[number];
+export type PageName = (typeof PAGE_NAMES)[number];
 
 export function isPageName(p: unknown): p is PageName {
   return PAGE_NAMES.includes(p as PageName);
